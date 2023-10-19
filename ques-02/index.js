dcb934f7630c6472d20bf7793b74c8bc8c42f693
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-10-19 18:43:19 +0530

    Add files via upload

---
+++ ques-02/index.js
@@ -0,0 +1,14 @@
+//Q.What are client side and server side in Javascript ? => v.imp
+/*
+
+1.mobiles , laptop , computer (are client)  ===connected with internet  which requests services from sever and server responds to it.
+2.client is Of (Html, css, javascript) Whereas server is of  c#, php , java our any languange
+3. resquest <==> response  relationship between them
+
+// CLIENT:
+ A client is a device, application, or software, component that requests and consumes resources from  a server.
+
+//SERVER:
+ A  server is a device, computer, or software application that provides services, resources  or function to clients.
+
+ */
