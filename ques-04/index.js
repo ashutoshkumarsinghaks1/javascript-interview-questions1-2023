8062411d671c276e6647cbd0e54fa6f78def5b27
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-10-21 21:16:51 +0530

    Add files via upload

---
+++ ques-04/index.js
@@ -0,0 +1,10 @@
+// Q. What is the type of variable in javascript when it is declared  without using the var , let or const keywords?
+if (true) {
+  variable = 10;
+  //here we declared variable without using var, let , const
+}
+console.log(variable);
+//Ouput : 10
+/*
+ans: "Var" is the implict(meaning always be found "default") type of variable when a variable is declared without var , let , const keywords.
+*/
