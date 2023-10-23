6ffd41d86e09a47c2dee46eb2ca4856d9776ad92
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-10-23 19:24:04 +0530

    Add files via upload

---
+++ ques-06/index.js
@@ -0,0 +1,17 @@
+//Q. what is JSON
+/*
+
+defn:
+1.JSON(Javascript Object Notation) is a light weight data interchange format.
+2. JSON consists of key-value pairs.
+3. example of json 
+{
+    "name":"Ashutosh",
+    "age":22,
+    "isStudent": true
+}
+These are used to exchange data between web API.
+
+USER => UI/CLIENT/FRONTENDEND => request =>  API/MIDDLEWARE
+API/MIDDLEWARE => response(JSON fomat)  =>  UI/CLIENT/FRONTENDEND => USER
+*/
