5e4b8f78a6390ebc8fbd320fcd0b2f04e04bb7e6
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-10-25 19:36:03 +0530

    Add files via upload

---
+++ ques-08/index.js
@@ -0,0 +1,45 @@
+//Q. What are data types in Javascript.
+/*
+defn:
+ A datatype determines the type of variable
+ 
+ ***** Types of data types in js ********
+ 1. Primitive => can hold only single value
+ 2. Non Primitive  => can hold multiple value
+ 
+*/
+
+/*
+Primitive
+1.Number
+2. Strings
+3.Booleans
+4.Undefined
+5. Null
+
+Non- Primitive
+1.Array
+2.Object
+3.Function
+4.Date
+5.RegExp
+*/
+
+//Number
+let age = 23;
+
+//String
+let name = " Ashutosh";
+
+//Boolean
+let isTrue = true;
+
+// Undefined
+let x;
+console.log(x);
+//output: undefined
+
+//Null
+let y = null;
+console.log(y);
+//output: null
