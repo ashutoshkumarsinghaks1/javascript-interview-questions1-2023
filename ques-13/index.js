0e91cb395c5e3ea6163643350107b6864dd07961
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-11-01 10:59:02 +0530

    Update index.js

--- ques-13/index.js
+++ ques-13/index.js
@@ -1,6 +1,14 @@
 //Q  What is operator precedence ?
+/*
+As per operator precendence operator with higher precdence are evaluated first.
+BODMAS RULE => BRACKET , ORDER , DIVISION , MULTIPLICATION , ADDITION , SUBTRACTION
+*/
 let a = 6;
 let b = 3;
 let c = 2;
 
-//Bracketof-Divison-Multiplication-Add-
+//Bracketof-Divison-Multiplication-Add-Sub
+let result = a + b * c + (a - b);
+
+console.log(result);
+//output:15
