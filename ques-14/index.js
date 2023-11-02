90d3f7a7954b4861c0b7825bcd1a519bf7a89912
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-11-02 19:10:36 +0530

    Add files via upload

---
+++ ques-14/index.js
@@ -0,0 +1,40 @@
+//Q.  What is the difference between  urinary , binary ,  ternary operator ?
+/*
+Operators are based on operands
+1. urinary operator => single operand
+2. binary operator  => two operand 
+3. ternary operator => three operand
+
+ */
+
+// 1. urinary
+let a = 5;
+
+// urinary operator
+//single operand
+let b = -a;
+console.log(b);
+//output : -5
+
+console.log(++a);
+//ouput: 6
+
+//binary operator
+let x = 10;
+let y = 3;
+
+//Binary operator
+//Two operand
+
+let z = x + y; //  two operand
+console.log(z);
+//ouput: 13
+
+// Ternary operator
+//Three operand
+
+// let result = condition ? "yes" : "no";
+// console.log(result);
+
+let result1 = 2 === 2 ? "yes" : "no";
+console.log(result1);
